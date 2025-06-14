6160653465948a1a71b811337dfe68e6bb2082a0
lau <lau@laudeMac-mini.local>
2025-06-14 16:02:46 +0800

    allowd CoSR

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  allowedDevOrigins: ['*'],
+  reactStrictMode: true
 };
 
-export default nextConfig;
+export default config;
